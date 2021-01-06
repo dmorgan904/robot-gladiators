5a123919ca5f4776b592019756ad194aa695a5c8
dmorgan904 <demetriusimorgan@gmail.com>
2021-01-05 23:22:43 -0500

    fix round number notification, defeat

--- assets/js/game.js
+++ assets/js/game.js
@@ -54,16 +54,28 @@ var fight = function(enemyName) {
 
     // check player's health
     if (playerHealth <= 0) {
-      window.alert(playerName + ' has died!');
-      // leave while() loop if player is dead
-      break;
+      window.alert("You have lost your robot in battle! Game Over!");
     } else {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
   }
 };
+
 for(var i= 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+  // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+    //pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+
+    //reset enemyHealth before starting new fight
+    enemyHealth = 50;
+
+    //use debugger to puase script from running and check what' going on at that moment in the code
+    //debugger;
+
+    //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+  } 
 }
